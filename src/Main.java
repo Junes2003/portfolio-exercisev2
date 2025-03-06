4420676366da459de8d0a23edec438a99561e6f3
Junes2003 <juel0001@stud.kea.dk>
2025-03-06 15:25:40 +0100

    ExerciseV2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import portfolio.Portfolio;
+import java.util.Scanner;
+import static menu.Menu.showMenu;
 
 public class Main {
     public static void main(String[] args) {
         // TODO: Opret en Scanner instance
+        Scanner scanner = new Scanner(System.in);
         // TODO: Opret en Portfolio instance
+        Portfolio portfolio = new Portfolio(200);
+
         // TODO: Show menu
+        showMenu(portfolio, scanner);
         // TODO: Luk scanneren
+        scanner.close();
     }
 }
--- src/market/Symbol.java
+++ src/market/Symbol.java
@@ -7,10 +7,19 @@ package market;
  */
 public enum Symbol {
     //TODO: Tilføj aktie ticker symboler (fx AAPL, GOOG osv)
+    AAPL,  // Apple
+    GOOG,  // Google
+    TSLA,  // Tesla
+    AMZN;  // Amazon
     ;
     public static boolean isValidSymbol(String symbol) {
         //TODO: Tjek om inputstrengen findes i de eksisterende symboler i enum'en
         //      Returner true hvis den eksistere ellers returner false
+        for (Symbol s : Symbol.values()) {
+            if (s.name().equalsIgnoreCase(symbol)) {
+                return true;
+            }
+        }
         return false;
     }
 }
